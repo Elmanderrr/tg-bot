97b499fd255783718de40ae1b661abf33a141ea7
Artem Korolov <artem.korolov@sensiaglobal.com>
2023-04-10 20:11:42 +0300

    refresh gitignore

--- src/parser.ts
+++
@@ -1,8 +0,0 @@
-export function parseMessage(msg: string) {
-  const parsed = msg.match(/\[.+?\]/g).map(str => str.replace(/[\[\]]/g, '')) || [];
-  console.log(parsed)
-  const [ type, token, result ] = parsed;
-  if(parsed?.length >= 2) {
-    console.log(type, token, result)
-  }
-}
